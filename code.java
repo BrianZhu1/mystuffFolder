89b99780e073b63581996188acd902f675309df9
Brian Zhu <brian.zhu@utexas.edu>
2015-08-05 23:40:56 -0500

    My second commit!

--- code.java
+++ code.java
@@ -1,6 +1,7 @@
-public class Test{
+public class Test
+{
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello World! This will be my second test!!!!! I love writing code!!!");
 	}
 }
